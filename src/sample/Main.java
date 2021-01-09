a42dceed9fec808e6fdcadc3808dc4f88e757b98
TrineKnudsen <trinesaknudsen@hotmail.com>
2021-01-09 14:05:29 +0100

    Størrelse på primarystage

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui/View/Main.fxml"));
         primaryStage.setTitle("Why so serious ?");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 650, 450));
         primaryStage.show();
     }
 
